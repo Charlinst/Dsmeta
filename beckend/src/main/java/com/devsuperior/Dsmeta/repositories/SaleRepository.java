c02fb229488432f41e2ffe8cc9c2b21a8642db26
charlinst <charlim001@gmail.com>
2022-09-09 20:45:31 -0300

    API test

---
+++ beckend/src/main/java/com/devsuperior/Dsmeta/repositories/SaleRepository.java
@@ -0,0 +1,9 @@
+package com.devsuperior.Dsmeta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.devsuperior.Dsmeta.entities.Sale;
+
+public interface SaleRepository extends JpaRepository<Sale, Long> {
+
+}
